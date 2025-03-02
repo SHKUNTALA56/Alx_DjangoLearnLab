8cefc53489d14df47b7296718e650fa17aec87e8
Brian Davis Kihiu <100779766+SHKUNTALA56@users.noreply.github.com>
2025-03-02 20:03:37 +0300

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,15 @@
 import django
 import os
 
+# Set up Django environment
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-
-
-# Query all books by a specific author
+# Query all books by a specific author (using filter, not get)
 def get_books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
+    return Book.objects.filter(author__name=author_name)  # ✅ Correct query
 
 # List all books in a specific library
 def get_books_in_library(library_name):
@@ -23,6 +22,6 @@ def get_librarian_for_library(library_name):
     return library.librarian
 
 if __name__ == "__main__":
-    print("Books by Author (J.K. Rowling):", get_books_by_author("J.K. Rowling"))
-    print("Books in Library (Central Library):", get_books_in_library("Central Library"))
+    print("Books by J.K. Rowling:", list(get_books_by_author("J.K. Rowling")))
+    print("Books in Central Library:", list(get_books_in_library("Central Library")))
     print("Librarian of Central Library:", get_librarian_for_library("Central Library"))
