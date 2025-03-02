9067fe71c9796f15cb4d85f53addfb837baec7b2
SHKUNTALA <briandaviskihiu@gmail.com>
2025-03-02 20:47:08 +0300

    update

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+    
 ]
 
